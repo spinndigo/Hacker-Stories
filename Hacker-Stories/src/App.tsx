bf6fde1ed02323be8ae9b50a2b702952d0f47d51
Joseph Spinosa <spinosajoseph@gmail.com>
2023-03-17 15:30:41 -0400

    data fetch p126

--- Hacker-Stories/src/App.tsx
+++ Hacker-Stories/src/App.tsx
@@ -8,6 +8,7 @@ import {
 import "./App.css";
 
 const title = "React";
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
 const welcome = { title: "React", greeting: "Hey" };
 
@@ -263,11 +264,12 @@ const App: React.FC<{}> = () => {
 
   useEffect(() => {
     dispatchStories({ type: Action.STORIES_FETCH_INIT });
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: Action.STORIES_FETCH_SUCCESS,
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: Action.STORIES_FETCH_FAILURE }));
