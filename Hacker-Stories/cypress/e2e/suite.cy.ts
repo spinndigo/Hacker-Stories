c93d04bd5adafb4e2518f574134b0d278651515a
Joseph Spinosa <spinosajoseph@gmail.com>
2024-08-17 12:44:12 -0400

    add cypress config

--- Hacker-Stories/cypress/e2e/suite.cy.ts
+++ Hacker-Stories/cypress/e2e/suite.cy.ts
@@ -4,12 +4,12 @@ const hits = "hits-list";
 const listHit = (id: string) => `hit-${id}`;
 const removelistItem = (id: string) => `remove-${id}`;
 
-describe("Connection to api", () => {
-  it("returns results when searching for graphql", () => {
-    cy.visit("https://example.cypress.io");
+describe("App Functions", () => {
+  it("opens the homepage", () => {
+    cy.visit("/");
   });
 
-  it("returns no results for bad search", () => {
-    cy.visit("https://example.cypress.io");
-  });
+  // it("returns no results for bad search", () => {
+  //   cy.visit("https://example.cypress.io");
+  // });
 });
